c0d8b4cca6b1a6c4ebcba90d19593c0e1451eabf
louis <louilou008@gmail.com>
2020-04-09 23:22:10 +0200

    Implementation of full rover movements (forward, backward ans rotates)

--- src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
@@ -5,8 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MarsRoverTest {
-    private final MarsRover rover = new MarsRoverImpl()
-        .initialize(Position.of(0, 0, Direction.NORTH));
+    private final MarsRover rover = new MarsRoverImpl(Position.of(0, 0, Direction.NORTH));
 
     @Test
     void move_forward() {
@@ -15,5 +14,50 @@ class MarsRoverTest {
         Assertions.assertThat(newPosition)
             .as("Rover position after \"f\" command")
             .isEqualTo(Position.of(0, 1, Direction.NORTH));
+    }
+
+    @Test
+    void move_backward() {
+        Position newPosition = rover.move("b");
+
+        Assertions.assertThat(newPosition)
+            .as("Rover position after \"f\" command")
+            .isEqualTo(Position.of(0, -1, Direction.NORTH));
+    }
+
+    @Test
+    void rotate_left() {
+        Position newPosition = rover.move("l");
+
+        Assertions.assertThat(newPosition)
+            .as("Rover position after \"f\" command")
+            .isEqualTo(Position.of(0, 0, Direction.WEST));
+    }
+
+    @Test
+    void rotate_right() {
+        Position newPosition = rover.move("r");
+
+        Assertions.assertThat(newPosition)
+            .as("Rover position after \"f\" command")
+            .isEqualTo(Position.of(0, 0, Direction.EAST));
+    }
+
+    @Test
+    void move_up_right() {
+        Position newPosition = rover.move("frfl");
+
+        Assertions.assertThat(newPosition)
+            .as("Rover position after \"f\" command")
+            .isEqualTo(Position.of(1, 1, Direction.NORTH));
+    }
+
+    @Test
+    void move_2_circles() {
+        Position newPosition = rover.move("frFRfrFrblBLbLbl");
+
+        Assertions.assertThat(newPosition)
+            .as("Rover position after \"f\" command")
+            .isEqualTo(Position.of(0, 0, Direction.NORTH));
     }
 }
