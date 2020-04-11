1d3e677f38185642f10511772918be4675b022f1
xinruk <numamuys@protonmail.com>
2020-04-11 07:25:13 -0400

    Implementation of the map and obstacles

--- src/main/java/com/esiea/tp4A/code/GameMap.java
+++ src/main/java/com/esiea/tp4A/code/GameMap.java
@@ -1,5 +1,6 @@
 package com.esiea.tp4A.code;
 
+import com.esiea.tp4A.domain.Direction;
 import com.esiea.tp4A.domain.PlanetMap;
 import com.esiea.tp4A.domain.Position;
 
@@ -9,17 +10,19 @@ public class GameMap implements PlanetMap {
 
     private Set<Position> obstacles;
 
-    //Constructor with parameter
+    //Constructor with parameter, to use a default map
     public GameMap(HashSet<Position> obstacles) {
         this.obstacles = obstacles;
     }
-
+    //Constructor without parameter to generate the map in game
     public GameMap() {
         obstacles = new HashSet<Position>();
     }
     public void addObstacles(Position position)
     {
-        obstacles.add(position);
+        //An obstacle with direction has no meaning
+        //To simplify the test, an obstacle can only have a NORTH direction
+        obstacles.add(Position.of(position.getX(), position.getY(), Direction.NORTH));
     }
 
     @Override
