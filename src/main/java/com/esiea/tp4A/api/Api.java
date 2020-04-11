3196cedaf68a57f8c10e2a4c68161120d19d9bb8
Gauthier Heiss <gauthier.heiss@gmail.com>
2020-04-12 01:10:32 +0200

    Ajout de l'interface "Api" dans un nouveau package

---
+++ src/main/java/com/esiea/tp4A/api/Api.java
@@ -0,0 +1,15 @@
+package com.esiea.tp4A.api;
+
+import com.esiea.tp4A.domain.MarsRover;
+import com.esiea.tp4A.domain.Position;
+
+import java.util.ArrayList;
+
+public interface Api {
+    Position getPosition(MarsRover rover);
+    ArrayList<Position> getRadarData(MarsRover rover, int range);
+    int getLaserRange(MarsRover rover);
+    Position move(MarsRover rover, String command);
+    void laserShoot(MarsRover rover);
+    boolean isPilotAlive(MarsRover rover);
+}
