29f79964b3ad2597e53559b251454164b4a95fdd
0xk4l <0xk4l@netc.eu>
2020-04-12 22:02:31 +0200

    [add] API first parsing rules

--- src/main/java/com/esiea/tp4A/api/HttpApi.java
+++ src/main/java/com/esiea/tp4A/api/HttpApi.java
@@ -1,26 +1,68 @@
 package com.esiea.tp4A.api;
 
-import com.sun.net.httpserver.HttpContext;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpServer;
-
+import com.esiea.tp4A.MarsRoverImpl;
+import com.esiea.tp4A.domain.MarsRover;
+import com.esiea.tp4A.domain.Position;
+import com.sun.net.httpserver.*;
+import org.json.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class HttpApi {
+
+    private static HashMap<Object, MarsRover> players;
+
     public static void main(String[] args) throws IOException {
-            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-            HttpContext context = server.createContext("/");
-            context.setHandler(HttpApi::handleRequest);
-            server.start();
+        players = new HashMap<>();
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpContext context = server.createContext("/");
+        context.setHandler(HttpApi::handleRequest);
+        server.start();
     }
 
     private static void handleRequest(HttpExchange exchange) throws IOException {
-            String response = "Hi there!";
-            exchange.sendResponseHeaders(200, response.getBytes().length);//response code and length
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+        URI requestURI = exchange.getRequestURI();
+
+        String path = requestURI.getPath();
+        String returnValue = "Incorrect value";
+        Integer HttpReturnCode = 404;
+
+        if (exchange.getRequestMethod().equals("GET")){
+            String playername = path.substring(path.lastIndexOf('/') + 1);
+            if (path.startsWith("/api/player/") && !playername.isEmpty()){
+                returnValue = "Get player " + path.substring(path.lastIndexOf('/') + 1);
+                HttpReturnCode = 200;
+            }
+        }
+        else if(exchange.getRequestMethod().equals("POST")){
+            String playername = path.substring(path.lastIndexOf('/') + 1);
+            if (path.startsWith("/api/player/") && !playername.isEmpty()){
+                returnValue = "Post player " + path.substring(path.lastIndexOf('/') + 1);
+                HttpReturnCode = 201;
+            }
+        }
+
+        exchange.sendResponseHeaders(HttpReturnCode, returnValue.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(returnValue.getBytes());
+        os.close();
     }
+
+/*    private void getPlayer(String playername){
+        MarsRover player = players.get(playername);
+        JSONObject json = new JSONObject(player);
+
+        System.out.print(json);
+    }
+
+    private void setPlayer(JSONObject player){
+
+    }*/
 }
