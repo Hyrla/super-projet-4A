60d207dcea28c0ade1bdcd0bd7634a2f5ef5c1d8
0xk4l <0xk4l@netc.eu>
2020-04-12 23:24:39 +0200

    [add] Making Getplayer and Setplayer usables

--- src/main/java/com/esiea/tp4A/api/HttpApi.java
+++ src/main/java/com/esiea/tp4A/api/HttpApi.java
@@ -1,68 +1,104 @@
 package com.esiea.tp4A.api;
 
+import com.esiea.tp4A.GameMap;
 import com.esiea.tp4A.MarsRoverImpl;
+import com.esiea.tp4A.domain.Direction;
 import com.esiea.tp4A.domain.MarsRover;
 import com.esiea.tp4A.domain.Position;
+import com.google.gson.Gson;
 import com.sun.net.httpserver.*;
-import org.json.*;
+import com.sun.source.tree.NewArrayTree;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 
 public class HttpApi implements Api{
-
     private static HashMap<String, MarsRover> players;
+    private static GameMap gameMap;
 
     public static void main(String[] args) throws IOException {
+        HttpApi api = new HttpApi();
+    }
+
+    private ArrayList<Position> generateRandomThings(int number, int range) {
+        ArrayList<Position> a = new ArrayList<>();
+        for(int i = 0; i < number; i++) {
+            a.add(Position.of(
+                (int)(-range + Math.random() * range * 2),
+                (int)(-range + Math.random() * range * 2),
+                Direction.NORTH));
+        }
+        return a;
+    }
+
+    public HttpApi() throws IOException {
         players = new HashMap<>();
+        gameMap = new GameMap(new HashSet<>(generateRandomThings(10, 50)));
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         HttpContext context = server.createContext("/");
-        context.setHandler(HttpApi::handleRequest);
+        context.setHandler(this::handleRequest);
         server.start();
     }
 
-    private static void handleRequest(HttpExchange exchange) throws IOException {
+    private void handleRequest(HttpExchange exchange) throws IOException {
         URI requestURI = exchange.getRequestURI();
 
         String path = requestURI.getPath();
         String returnValue = "Incorrect value";
-        Integer HttpReturnCode = 404;
+        int HttpReturnCode = 404;
 
-        if (exchange.getRequestMethod().equals("GET")){
+        if (exchange.getRequestMethod().equals("GET")) {
             String[] segments = path.split("/");
             String playername = segments[3];
 
-            if (path.startsWith("/api/player/") && !playername.isEmpty()){
-                returnValue = "Get player " + playername;
-                HttpReturnCode = 200;
+            if (path.startsWith("/api/player/") && !playername.isEmpty()) {
+                if (players.containsKey(playername)) {
+                    returnValue = getMarsRoverJsonData(players.get(playername));
+                    HttpReturnCode = 200;
+                } else {
+                    returnValue = "Player " + playername + " don't exist !";
+                    HttpReturnCode = 404;
+                }
             }
         }
+
         else if(exchange.getRequestMethod().equals("POST")){
             String[] segments = path.split("/");
             String playername = segments[3];
 
-            if (path.startsWith("/api/player/") && !playername.isEmpty()){
-                returnValue = "Post player " + playername;
-                HttpReturnCode = 201;
+            if (path.startsWith("/api/player/") && !playername.isEmpty()) {
+                if (players.containsKey(playername)) {
+                    returnValue = "Player " + playername + " already used !";
+                    HttpReturnCode = 409;
+                } else {
+                    returnValue = getMarsRoverJsonData(
+                        createMarsRoverFromName(playername, gameMap, 1));
+                    HttpReturnCode = 201;
+                }
             }
         }
+
         else if (exchange.getRequestMethod().equals("PATCH")){
             String[] segments = path.split("/");
-            String playername = segments[3];
-            String command = segments[4];
-            System.out.print(playername + " " + command);
+                String playername = segments[3];
+                String command = segments[4];
+                System.out.print(playername + " " + command);
 
-            if (path.startsWith("/api/player/") && !playername.isEmpty() && !command.isEmpty()){
-                returnValue = "Patch command " + command +
-                    " to player " + playername;
-                HttpReturnCode = 201;
+                if (path.startsWith("/api/player/") && !playername.isEmpty() && !command.isEmpty()) {
+                    returnValue = "Patch command " + command +
+                        " to player " + playername;
+                    HttpReturnCode = 201;
+                }
             }
-        }
+
 
         exchange.sendResponseHeaders(HttpReturnCode, returnValue.getBytes().length);
         OutputStream os = exchange.getResponseBody();
@@ -100,14 +136,24 @@ public class HttpApi implements Api{
         return false;
     }
 
-/*    private void getPlayer(String playername){
-        MarsRover player = players.get(playername);
-        JSONObject json = new JSONObject(player);
-
-        System.out.print(json);
+    private String getMarsRoverJsonData(MarsRover rover) {
+        Gson gson = new Gson();
+        return gson.toJson(rover);
     }
 
-    private void setPlayer(JSONObject player){
+    private MarsRover createMarsRoverFromName(String name, GameMap gm, int laserRange) {
+        int max = 50;
+        int min = -50;
+        int x,y = 0;
 
-    }*/
+        do {
+            x = (int)(Math.random()*((max-min)+1))+min;
+            y = (int)(Math.random()*((max-min)+1))+min;
+        } while (gameMap.isPositionFree(x,y));
+
+
+        MarsRoverImpl mr = new MarsRoverImpl(x, y, Direction.NORTH, gameMap, laserRange);
+        players.put(name, mr);
+        return mr;
+    }
 }
--- src/test/java/com/esiea/tp4A/api/HttpApiTest.java
+++ src/test/java/com/esiea/tp4A/api/HttpApiTest.java
@@ -2,10 +2,12 @@ package com.esiea.tp4A.api;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 public class HttpApiTest {
 
     @Test
-    void createWebserver(){
+    void createWebserver() throws IOException {
         HttpApi httpapi = new HttpApi();
     }
 }
