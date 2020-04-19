7c41d581a54fce4d5ffc41d38c55c50df18d2764
LeparouxTitouan <titouan.leparoux@free.fr>
2020-04-19 21:51:07 +0200

    [edit] name function called

--- src/test/java/com/esiea/tp4A/domain/GameMapTest.java
+++ src/test/java/com/esiea/tp4A/domain/GameMapTest.java
@@ -52,7 +52,7 @@ public class GameMapTest {
     }
 
     @Test
-    void getRealy(){
+    void getRealY(){
         GameMap gameMap = new GameMap(new HashSet<>(Arrays.asList(
             Position.of(2, 2, Direction.NORTH)
         )), 100);
