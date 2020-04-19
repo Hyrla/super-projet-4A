c404381a45df1d6102152c96066515813901e1a5
Gauthier Heiss <gauthier.heiss@gmail.com>
2020-04-19 21:52:08 +0200

    Add test for webserver

--- src/test/java/com/esiea/tp4A/api/HttpApiTest.java
+++ src/test/java/com/esiea/tp4A/api/HttpApiTest.java
@@ -1,13 +1,55 @@
 package com.esiea.tp4A.api;
 
 import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+import org.springframework.boot.SpringApplication;
 
-import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.http.HttpRequest;
 
 public class HttpApiTest {
 
     @Test
-    void createWebserver() throws IOException {
-        HttpApi httpapi = new HttpApi();
+    void testWebserver() {
+        SpringApplication.run(RestAPI.class);
+        try {
+            URL url = new URL("http://localhost:8080/api/players");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            Assertions.assertThat(con.getResponseCode() == 200);
+        } catch (Exception e) {
+            Assertions.assertThat(false);
+        }
+
+        try {
+            URL url = new URL("http://localhost:8080/api/player/joueur");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            Assertions.assertThat(con.getResponseCode() == 200);
+        } catch (Exception e) {
+            Assertions.assertThat(false);
+        }
+
+        try {
+            URL url = new URL("http://localhost:8080/api/player/joueur");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            Assertions.assertThat(con.getResponseCode() == 200);
+        } catch (Exception e) {
+            Assertions.assertThat(false);
+        }
+
+        try {
+            URL url = new URL("http://localhost:8080/api/player/joueur");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            URL url2 = new URL("http://localhost:8080/api/player/joueur");
+            HttpURLConnection con2 = (HttpURLConnection) url2.openConnection();
+            con2.setRequestMethod("POST");
+            Assertions.assertThat(con.getResponseCode() == 409);
+        } catch (Exception e) {
+            Assertions.assertThat(false);
+        }
     }
 }
